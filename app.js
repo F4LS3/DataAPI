5db564db71cb185a6cb708d9b0f4d6f455e6fc35
Linus Ende <endelinus920@gmail.com>
2020-08-14 17:11:19 +0200

    + Added secure file upload and config

--- app.js
+++ app.js
@@ -2,25 +2,29 @@ console.clear();
 
 const express = require("express");
 const app = express();
+const fileUpload = require("express-fileupload");
 const mysql = require("mysql");
 const fs = require("fs");
-const { dir } = require("console");
+
+const config = require("./config.json");
+const { isArray } = require("util");
 
 const conn = mysql.createConnection({
-    host: "localhost",
-    user: "linus",
-    password: "Anf10@ng20De",
-    database: "dataapi"
+    host: config.database.host,
+    user: config.database.user,
+    password: config.database.password,
+    database: config.database.database
 });
 
 conn.connect(err => {
-    if (err) return console.error(`[ERROR] ${err}`);
+    if (err) return console.error(`[ERROR] Failed to connect to database: ${err}`);
     console.log(`[INFO] Established connection to database`);
 });
 
 app.use(express.static(`${__dirname}/public`));
+app.use(fileUpload({ createParentPath: true }));
 
-app.get('/request', (req, res) => {
+app.get('/get', (req, res) => {
     let token = req.headers.token;
 
     if (token == null || token == undefined) return res.sendStatus(401);
@@ -35,7 +39,7 @@ app.get('/request', (req, res) => {
 
         if (tokenRdp != null) {
             if (tokenRdp.token != null) {
-                let urlParams = req.url.replace("/request?", "").toLowerCase().split("+");
+                let urlParams = req.url.replace("/get?", "").toLowerCase().split("+");
                 let dir, file;
                 urlParams.forEach(param => {
                     let params = param.split("=");
@@ -56,16 +60,17 @@ app.get('/request', (req, res) => {
 
                     if (hasAccess(tokenRdp.access, fileRdp.access)) {
                         const path = `${__dirname}/data/${dir}/${file}`;
-                        if(fs.existsSync(path)) {
+
+                        if (fs.existsSync(path)) {
                             res.status(200).sendFile(path);
                             return console.log(`[INFO] File '${file}' was requested by '${tokenRdp.token}' (authorized)`);
-                            
+
                         } else {
                             return res.status(404).send("file not found");
                         }
 
                     } else {
-                        console.log(`[INFO] File '${file}' was requested by '${tokenRdp.token}' (unauthorized & stopped)`);
+                        console.log(`[INFO] File '${file}' was requested by '${tokenRdp.token}' but wasn't permitted and therefore not sent`);
                         return res.sendStatus(401);
                     }
                 });
@@ -80,9 +85,82 @@ app.get('/request', (req, res) => {
     });
 });
 
+app.post('/post', (req, res) => {
+    let token = req.headers.token;
+
+    if (token == null || token == undefined) return res.sendStatus(401);
+
+    conn.query(`SELECT * FROM \`dataapi_tokens\` WHERE token='${token}'`, (err, result) => {
+        if (err) {
+            res.status(400).send(`error: ${err}`);
+            return console.error(`[ERROR] ${err}`);
+        }
+
+        let tokenRdp = result[0];
+
+        if (tokenRdp != null) {
+            if (tokenRdp.token != null) {
+                let urlParams = req.url.replace("/post?", "").toLowerCase().split("+");
+                let dir, file;
+                urlParams.forEach(param => {
+                    let params = param.split("=");
+
+                    let request = params[0];
+                    let value = params[1];
+
+                    request == "dir" ? dir = value : file = value;
+                });
+
+                conn.query(`SELECT * FROM \`dataapi_access\` WHERE dir = '${dir}'`, (err, result) => {
+                    if (err) {
+                        res.status(400).send(`error: ${err}`);
+                        return console.error(`[ERROR] ${err}`);
+                    }
+
+                    let fileRdp = result[0];
+
+                    if (!req.files) return res.status(400).send("no file specified");
+
+                    if (hasAccess(tokenRdp.access, fileRdp.access)) {
+                        let data = [];
+
+                        if (isArray(req.files.files)) {
+                            let files = Object.keys(req.files.files);
+
+                            files.forEach(key => {
+                                let file = req.files.files[key];
+
+                                file.mv(`${__dirname}/data/${dir}/${file.name}`);
+                                data.push({
+                                    name: file.name,
+                                    mimetype: file.mimetype,
+                                    size: file.size
+                                });
+                            });
+
+                        } else {
+                            let file = req.files.files;
+
+                            file.mv(`${__dirname}/data/${dir}/${file.name}`);
+                            data.push({
+                                name: file.name,
+                                mimetype: file.mimetype,
+                                size: file.size
+                            });
+                        }
+                        return res.status(200).send({ message: "Files uploaded successful", data: data });
+                    
+                    } else {
+                        return res.sendStatus(401);
+                    }
+                });
+            }
+        }
+    });
+});
+
 function hasAccess(tokenAccess, fileAccess) {
-    if (tokenAccess == "*") return true;
-    return tokenAccess == fileAccess;
+    return tokenAccess == "*" ? true : tokenAccess == fileAccess;
 }
 
 setInterval(() => {
